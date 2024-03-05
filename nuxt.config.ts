ee4047f42b44cc024e48b05ac4f5c6fe3524b480
Jeremy Hale <jeremy.hale@gmail.com>
2024-03-05 09:04:51 +0900

    Add firebase config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,17 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
-})
+  devtools: { enabled: true },
+  ssr: false,
+  modules: ["nuxt-vuefire"],
+  vuefire: {
+    config: {
+      apiKey: "AIzaSyDyTmnM9K1BNABWyeVhL2IIxO7xcJAstms",
+      authDomain: "nuxt-vuefire-2436b.firebaseapp.com",
+      projectId: "nuxt-vuefire-2436b",
+      storageBucket: "nuxt-vuefire-2436b.appspot.com",
+      messagingSenderId: "906522659037",
+      appId: "1:906522659037:web:4b0612cb07278f024a7334",
+      measurementId: "G-T93BQEQ6H6",
+    },
+  },
+});
