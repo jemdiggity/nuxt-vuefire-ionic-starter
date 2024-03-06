476bc276fd049fc6e663e30c30dabb6bcaf9ce5a
Jeremy Hale <jeremy.hale@gmail.com>
2024-03-06 09:24:07 +0900

    Add test pages devices & index

---
+++ pages/devices.vue
@@ -0,0 +1,22 @@
+import { IonBackButton } from '#build/components'; import { IonButtons } from
+'#build/components';
+<template>
+  <IonPage>
+    <IonHeader>
+      <IonToolbar>
+        <IonButtons slot="start">
+          <IonBackButton></IonBackButton>
+        </IonButtons>
+
+        <IonTitle> Devices </IonTitle>
+      </IonToolbar>
+    </IonHeader>
+    <IonContent class="ion-padding">
+      <IonButton>Logout</IonButton>
+      <IonButton router-link="/">Home</IonButton>
+      <IonCard>
+        <IonCardContent>blurg</IonCardContent>
+      </IonCard>
+    </IonContent>
+  </IonPage>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <IonPage>
+    <IonHeader>
+      <IonToolbar>
+        <IonTitle> Home </IonTitle>
+      </IonToolbar>
+    </IonHeader>
+    <IonContent class="ion-padding">
+      <IonButton>Logout</IonButton>
+      <IonButton router-link="/devices">Devices</IonButton>
+      <IonCard>
+        <IonCardContent>blurg</IonCardContent>
+      </IonCard>
+    </IonContent>
+  </IonPage>
+</template>
